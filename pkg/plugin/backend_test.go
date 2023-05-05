c9ea299719861a18f04534e6889e6400d552bb08
Niamh Thornbury <nthornbury@confluent.io>
2023-05-05 10:11:12 +0100

    removing duplicate code

--- pkg/plugin/backend_test.go
+++ pkg/plugin/backend_test.go
@@ -3,10 +3,10 @@ package plugin
 import (
 	"context"
 	"github.com/hashicorp/go-hclog"
-	"os"
-	"testing"
 	"github.com/hashicorp/vault/sdk/logical"
 	"github.com/stretchr/testify/require"
+	"os"
+	"testing"
 )
 
 const (
@@ -110,48 +110,6 @@ func (testEnv *testEnv) ReadToken(testing *testing.T) {
 	require.NotNil(testing, response.Secret)
 	if testToken, ok := response.Secret.InternalData["token"]; ok {
 		testEnv.SecretToken = testToken.(string)
-	}
-}
-
-// AddRole adds a role for CCloud Cluster API keys.
-func (e *testEnv) AddRole(t *testing.T) {
-	req := &logical.Request{
-		Operation: logical.UpdateOperation,
-		Path:      "role/test-cluster-role",
-		Storage:   e.Storage,
-		Data: map[string]interface{}{
-			"owner": e.Owner,
-		},
-	}
-	resp, err := e.Backend.HandleRequest(e.Context, req)
-	require.Nil(t, resp)
-	require.Nil(t, err)
-}
-
-// ReadKey retrieves the Cluster API key based on a Vault role.
-func (e *testEnv) ReadToken(t *testing.T) {
-	req := &logical.Request{
-		Operation: logical.ReadOperation,
-		Path:      "creds/test-cluster-token",
-		Storage:   e.Storage,
-	}
-	resp, err := e.Backend.HandleRequest(e.Context, req)
-	require.Nil(t, err)
-	require.NotNil(t, resp)
-
-	if t, ok := resp.Data["token"]; ok {
-		e.Keys = append(e.Keys, t.(string))
-	}
-	require.NotEmpty(t, resp.Data["token"])
-
-	if e.SecretToken != "" {
-		require.NotEqual(t, e.SecretToken, resp.Data["token"])
-	}
-
-	// collect secret IDs to revoke at end of test
-	require.NotNil(t, resp.Secret)
-	if t, ok := resp.Secret.InternalData["token"]; ok {
-		e.SecretToken = t.(string)
 	}
 }
 
